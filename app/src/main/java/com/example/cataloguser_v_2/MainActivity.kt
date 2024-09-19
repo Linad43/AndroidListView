e008af3f411a0a42fee71fe908e6eb7f61749c2b
Linad <merzlikindaniil97@gmail.com>
2024-09-19 10:37:08 +0300

    add toolbar_menu.xml, add func to menu

--- app/src/main/java/com/example/cataloguser_v_2/MainActivity.kt
+++ app/src/main/java/com/example/cataloguser_v_2/MainActivity.kt
@@ -1,12 +1,18 @@
 package com.example.cataloguser_v_2
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var toolbar: Toolbar
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +22,24 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        toolbar = findViewById(R.id.toolbar_main)
+
+        setSupportActionBar(toolbar)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.exit -> {
+                finish()
+            }
+        }
+        return true
     }
 }
